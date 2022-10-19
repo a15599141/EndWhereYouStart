6419b80bc8e2206a1ef3f70441e69a5b43bcf622
Zijia <451246352@qq.com>
2022-10-20 04:14:24 +0800

    no message

--- Assets/Scripts/HealthSystem/Damage/HammerDamage.cs
+++ Assets/Scripts/HealthSystem/Damage/HammerDamage.cs
@@ -9,7 +9,6 @@ namespace CodeMonkey.HealthSystemCM {
     public class HammerDamage: MonoBehaviour {
 
         [SerializeField] private GameObject getHealthSystemGameObject;
-        HealthSystem hs;
         public void Start() {
             //HealthSystem.TryGetHealthSystem(getHealthSystemGameObject, out HealthSystem healthSystem, true);
             //healthSystem.Damage(10);
@@ -21,8 +20,7 @@ namespace CodeMonkey.HealthSystemCM {
             if (Input.GetMouseButtonDown(0))
             {
                 HealthSystem.TryGetHealthSystem(getHealthSystemGameObject, out HealthSystem healthSystem, true);
-                hs = healthSystem;
-                hs.Damage(10);
+                healthSystem.Damage(10);
                 print("鼠标左键被按下！");
             }
             
